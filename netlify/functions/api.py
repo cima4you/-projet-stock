fe5410db4e40e1815afe615ff8c20e759ef3a7ab
rachid bazighe <bazigherachid@gmail.com>
2026-06-24 20:35:46 +0100

    Fix Netlify function path + runtime.txt

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,11 +1,13 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
+BASE = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.chdir(BASE)
+sys.path.insert(0, BASE)
 
 from serverless_wsgi import handle_request
-from main import app
+from main import app as application
 
 
 def handler(event, context):
-    return handle_request(app, event, context)
+    return handle_request(application, event, context)
